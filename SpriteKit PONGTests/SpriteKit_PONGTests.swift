7370094ed6618fdccb9a613775c06bc834749b63
Michael Linker <mlinker2231@jh-mba158-08.local>
2019-04-15 11:28:14 -0500

    Initial Commit

---
+++ SpriteKit PONGTests/SpriteKit_PONGTests.swift
@@ -0,0 +1,34 @@
+//
+//  SpriteKit_PONGTests.swift
+//  SpriteKit PONGTests
+//
+//  Created by Michael Linker on 4/15/19.
+//  Copyright © 2019 Michael Linker. All rights reserved.
+//
+
+import XCTest
+@testable import SpriteKit_PONG
+
+class SpriteKit_PONGTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
